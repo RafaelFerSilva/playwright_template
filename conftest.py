4143a92825d0bed3f6e792c7cba2aa5a317fbe53
rafaelsilva <todosomtodavoz@gmail.com>
2025-04-09 09:39:18 -0300

    refact: remove context config

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 from typing import Generator
 
 @pytest.fixture(scope="function")
@@ -8,11 +8,3 @@ def page(browser) -> Generator[Page, None, None]:
     page.set_viewport_size({"width": 1920, "height": 1080})
     yield page
     page.close()
-
-@pytest.fixture(scope="session")
-def browser_context_args(browser_context_args):
-    return {
-        **browser_context_args,
-        "viewport": {"width": 1920, "height": 1080},
-        "record_video_dir": "videos/"
-    }
