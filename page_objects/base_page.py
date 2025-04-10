fad9355171f12b92d1a66027ce10d4d2242bab6d
rafaelsilva <todosomtodavoz@gmail.com>
2025-04-10 08:48:48 -0300

    feat: add test steps

--- page_objects/base_page.py
+++ page_objects/base_page.py
@@ -1,29 +1,38 @@
+import allure
 from playwright.sync_api import Page
 
 class BasePage:
     def __init__(self, page: Page):
         self.page = page
 
+    @allure.step("Navigate to Page")
     def navigate_to(self, url: str):
         self.page.goto(url)
 
+    @allure.step("Get Element")
     def get_element(self, selector: str):
         return self.page.locator(selector)
     
+    @allure.step("Get Page Title")
     def get_page_title(self) -> str:
         return self.page.title()
 
+    @allure.step("Click Element")
     def click_element(self, selector: str):
         self.get_element(selector).click()
-
+    
+    @allure.step("Fill Text")
     def fill_text(self, selector: str, text: str):
         self.get_element(selector).fill(text)
 
+    @allure.step("Get Element")
     def get_text(self, selector: str) -> str:
         return self.get_element(selector).text_content()
 
+    @allure.step("Is Visible")
     def is_visible(self, selector: str) -> bool:
         return self.get_element(selector).is_visible()
 
+    @allure.step("Wait For Element")
     def wait_for_element(self, selector: str, timeout: int = 5000):
         self.page.wait_for_selector(selector, timeout=timeout)
--- tests/test_home.py
+++ tests/test_home.py
@@ -1,13 +1,22 @@
 import pytest
+import allure
 from page_objects.home_page import HomePage
+from utils.asserts import BaseAsserts
 
 class TestHome:
     @pytest.fixture
-    def home_page(self, page):
+    def home_page(self, page) -> HomePage:
         return HomePage(page)
 
-    def test_check_page_title(self, home_page):
+    @allure.title('Check Page Title')
+    def test_check_page_title(self, home_page: HomePage):
         home_page.navigate_to("https://demoqa.com/")
         title = home_page.get_page_title()
         assert title == "DEMOQA"
+    
+    @allure.title('Check Page Error')
+    def test_check_page_title2(self, home_page: HomePage):
+        home_page.navigate_to("https://demoqa.com/")
+        title = home_page.get_page_title()
+        BaseAsserts.assert_string_value(self, "DEMOQ", title)
 
